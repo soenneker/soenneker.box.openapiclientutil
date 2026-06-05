00351abe2da3c7d3e81d3c18ece1169be7fdfd28
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:10 -0500

    xml commenting

--- src/Soenneker.Box.OpenApiClientUtil/Abstract/IBoxOpenApiClientUtil.cs
+++ src/Soenneker.Box.OpenApiClientUtil/Abstract/IBoxOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Box.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IBoxOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<BoxOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
