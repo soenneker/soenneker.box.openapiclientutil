6d4ac537f0e71fe4ede827cc08572ed697783d89
soenneker <jake@soenneker.com>
2026-09-03 22:01:57 -0500

    Add interface inheritdoc references

--- src/Soenneker.Box.OpenApiClientUtil/BoxOpenApiClientUtil.cs
+++ src/Soenneker.Box.OpenApiClientUtil/BoxOpenApiClientUtil.cs
@@ -14,6 +14,7 @@ using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Box.OpenApiClientUtil;
 
+/// <inheritdoc cref="IBoxOpenApiClientUtil" />
 public sealed class BoxOpenApiClientUtil : IBoxOpenApiClientUtil
 {
     private readonly AsyncSingleton<ClientState> _client;
